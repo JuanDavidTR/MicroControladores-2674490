201aa0f27cb38d3c72c5a3406c4ff662f7528ecf
JuanDavidTR <148662842+JuanDavidTR@users.noreply.github.com>
2023-10-29 14:38:05 -0500

    semaforo_21.ino

--- semaforo_21.ino
+++ semaforo_21.ino
@@ -1,57 +1,65 @@
 const int pinRojo = 7;
 const int pinAmarillo = 6;
 const int pinVerde = 5;
-const int pinBotonParo = 11;
-const int pinBotonMantenimiento = 10;
-const int pinPotenciometro = A0;
+const int pinBotonParo = 4;
+const int pinBotonMantenimiento = 3;
+const int potenciometroPin = A0;
 
+bool mantenimientoActivo = false;
+bool amarilloEncendido = false;
 int contadorBombilloRojo = 0;
 
 void setup() {
   pinMode(pinRojo, OUTPUT);
   pinMode(pinAmarillo, OUTPUT);
   pinMode(pinVerde, OUTPUT);
-  pinMode(pinBotonParo, INPUT_PULLUP);
-  pinMode(pinBotonMantenimiento, INPUT_PULLUP);
-  Serial.begin(9600);  // Inicializar la comunicación serial a 9600 bps
+  pinMode(pinBotonParo, INPUT);
+  pinMode(pinBotonMantenimiento, INPUT);
+  Serial.begin(9600);
+}
+
+void Secuencia_1(int delayFactor) {
+  digitalWrite(pinRojo, HIGH);
+  digitalWrite(pinAmarillo, LOW);
+  digitalWrite(pinVerde, LOW);
+  delay(5000 / delayFactor);
+
+  digitalWrite(pinRojo, HIGH);
+  digitalWrite(pinAmarillo, HIGH);
+  digitalWrite(pinVerde, LOW);
+  delay(1000 / delayFactor);
+
+  digitalWrite(pinRojo, LOW);
+  digitalWrite(pinAmarillo, LOW);
+  digitalWrite(pinVerde, HIGH);
+  delay(3000 / delayFactor);
+
+  digitalWrite(pinRojo, LOW);
+  digitalWrite(pinAmarillo, HIGH);
+  digitalWrite(pinVerde, LOW);
+  delay(1000 / delayFactor);
 }
 
 void loop() {
-  contadorBombilloRojo++;
-    Serial.print("Bombillo rojo encendido. Contador: ");
-    Serial.println(contadorBombilloRojo);
-  // Leer el valor del potenciómetro para ajustar los tiempos del semáforo
-  int valorPotenciometro = analogRead(pinPotenciometro);
-
-  // Ciclo del semáforo con tiempos ajustados por el potenciómetro
-  digitalWrite(pinRojo, HIGH);
-  delay(500 / (valorPotenciometro / 1023.0));
-
-  digitalWrite(pinAmarillo, HIGH);
-  delay(100 / (valorPotenciometro / 1023.0));
-  digitalWrite(pinRojo, LOW);
-
-  digitalWrite(pinVerde, HIGH);
-  delay(300 / (valorPotenciometro / 1023.0));
-  digitalWrite(pinAmarillo, LOW);
-  digitalWrite(pinVerde, LOW);
-
-  // Amarillo intermitente durante 15 segundos o hasta presionar el botón de mantenimiento
-  int tiempoAmarilloIntermitente = 15000 / (valorPotenciometro / 1023.0);
-  unsigned long tiempoInicioAmarillo = millis();
-
-  while (millis() - tiempoInicioAmarillo < tiempoAmarilloIntermitente && digitalRead(pinBotonMantenimiento) == HIGH) {
-    digitalWrite(pinAmarillo, HIGH);
-    delay(275 / (valorPotenciometro / 1023.0));
-    digitalWrite(pinAmarillo, LOW);
-    delay(275 / (valorPotenciometro / 1023.0));
-  }
-
-  digitalWrite(pinAmarillo, LOW);
-
-  // Detener el ciclo al presionar el botón de paro
-  while (digitalRead(pinBotonParo) == HIGH) {
-    
-  
+  int delayFactor = map(analogRead(potenciometroPin), 0, 1023, 1, 10); // Mapear el valor analógico del potenciómetro a un factor de retardo
+  if (digitalRead(pinBotonMantenimiento) == HIGH) {
+    mantenimientoActivo = true;
+    digitalWrite(pinRojo, LOW);
+    digitalWrite(pinVerde, LOW);
+    digitalWrite(pinAmarillo, amarilloEncendido ? LOW : HIGH);
+    delay(500 / delayFactor);  // Controla la velocidad del titileo del amarillo
+    amarilloEncendido = !amarilloEncendido;
+  } else {
+    mantenimientoActivo = false;
+    if (!amarilloEncendido && digitalRead(pinBotonParo) == LOW) {
+      Secuencia_1(delayFactor);
+      contadorBombilloRojo++; // Incrementa el contador del bombillo rojo
+      Serial.print("Bombillo Rojo encendido: ");
+      Serial.println(contadorBombilloRojo);
+    } else {
+      digitalWrite(pinRojo, LOW);
+      digitalWrite(pinAmarillo, LOW);
+      digitalWrite(pinVerde, LOW);
+    }
   }
 }
